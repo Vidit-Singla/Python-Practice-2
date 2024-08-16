7315c4ee23483e460916f810a1cf3583266627cb
Vidit-Singla <singlavidit2428@gmail.com>
2024-08-16 13:46:16 +0530

    Add files via upload

---
+++ smiths.py
@@ -0,0 +1,17 @@
+def smith(s):
+    l= s.split()
+    
+    l1=[]
+    for i in l:
+        x=i.title()
+        l1. append(x)
+    
+    s1=''
+    for j in l1:
+        s1+=j
+        s1+=' '
+    print(s1)
+
+    
+smith("how can mirrors be real if our eyes aren't")
+    
