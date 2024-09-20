830c62135f117e374207520e371b7d4432ffe8a9
Vidit-Singla <singlavidit2428@gmail.com>
2024-09-20 22:13:14 +0530

    Add files via upload

--- Prime_Reversal.py
+++ Prime_Reversal.py
@@ -1,38 +1,15 @@
 t = int(input())
 f = []
-def primechecker(n):
-    x = True
-    for i in range(2,n):
-        if n%i == 0:
-            x = False
-    return(x)
-
-
-
 
 for __ in range(t):
     n = int(input())
     a = input()
     b = input()
-    c = 1
-    l = []
-    if a == b:
-        f.append('YES')
-        break
-    else:
-        for i in range(n):
-            if a[i] != b[i]:
-                c += 1
-            else:
-                if c!=0:
-                    l.append(c)
-                c = 0
-    if sum(l) == True:
+
+    if a.count('1') == b.count('1'):
         f.append('YES')
     else:
         f.append('NO')
 
 for __ in f:
     print(__)
-
-
