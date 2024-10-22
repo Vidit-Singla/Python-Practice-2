66c81824213a4490508f8ad337ad5d7c6adc8210
Vidit-Singla <singlavidit2428@gmail.com>
2024-10-22 16:28:01 +0530

    Add files via upload

---
+++ That_is_my_Score.py
@@ -0,0 +1,20 @@
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    d = {}
+    l = []
+    for i in range(5):
+        a,b = map(int,input().split())
+        if a <= 8:
+            l.append([a,b])
+            d[a] = 0
+    for i in l:
+        if d[i[0]] < i[1]:
+            d[i[0]] = i[1]
+    s = 0
+    for i in d:
+        s += d[i]
+    print(s)
+    
+
+
