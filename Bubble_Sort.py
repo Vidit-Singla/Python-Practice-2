067545f8761dcee44b2d83cee2d3cf7e555e0813
Vidit-Singla <singlavidit2428@gmail.com>
2024-11-05 16:39:24 +0530

    Add files via upload

---
+++ Bubble_Sort.py
@@ -0,0 +1,9 @@
+def bubble_sort(arr):
+    for i in range(len(arr)-1):
+        for j in range(len(arr)-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j] , arr[j+1] = arr[j+1] , arr[j]
+    
+    return(arr)
+
+print(bubble_sort([3,2,4,1,0]))
--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,9 +1,9 @@
 def ssort(a,n):
     for i in range(0,n):
         mini = i
-        for j in range(i,n): 
+        for j in range(i+1,n): 
             if a[j]<a[mini]:
                 mini = j
             a[i],a[mini] = a[mini],a[i]
     return a
-print(ssort([-1 ,-2 ,-5 ,10 ,9 ,8 ,7],7))
+print(ssort(list(map(int,input("ENTER LIST: ").split())),int(input("ENTER LENGTH OF LIST: "))))
