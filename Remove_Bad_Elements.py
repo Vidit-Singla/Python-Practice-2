4d5bf0004626d328cca8c63b030616bf3edb5c4e
Vidit-Singla <singlavidit2428@gmail.com>
2024-11-07 13:41:10 +0530

    Add files via upload

---
+++ Remove_Bad_Elements.py
@@ -0,0 +1,13 @@
+# for _ in range(int(input())):
+#     n = int(input())
+#     l = list(map(int,input().split()))
+#     d = {}
+#     for i in l:
+#         if i in d:
+#             d[i] += 1
+#         else:
+#             d[i] = 1
+#     m = max(d.values())
+#     print(n-m)
+l = [1,2,3]
+print(sum(l))
